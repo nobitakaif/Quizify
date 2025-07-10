a086f2b534cc3a03c3b19cd5e87a87c0faba9462
mohd kaif <mk2818356@gmail.com>
2025-07-10 23:44:52 +0530

    fixing deploying error

--- app/(main)/mcq/(rank)/rank/[pageNotFound]/page.tsx
+++ app/(main)/mcq/(rank)/rank/[pageNotFound]/page.tsx
@@ -1,20 +1,26 @@
-import FuzzyText from "@/components/pageNotFound";
+// import FuzzyText from "@/components/pageNotFound";
 
-export default function PageNotFound({params}:{params:{pageNotFound:string}}){
-    const pathName = params.pageNotFound // getting the path 
-    return <div className="h-screen  flex justify-center items-center flex-col">
-        <FuzzyText 
-            baseIntensity={0.2} 
-            >
-            404  Page Not Found 
-        </FuzzyText>
+// export default function PageNotFound({params}:{params:{pageNotFound:string}}){
+//     const pathName = params.pageNotFound // getting the path 
+//     return <div className="h-screen  flex justify-center items-center flex-col">
+//         <FuzzyText 
+//             baseIntensity={0.2} 
+//             >
+//             404  Page Not Found 
+//         </FuzzyText>
 
-        <br/>
-         <FuzzyText 
-            baseIntensity={0.2} 
-            >
-            {pathName}
-        </FuzzyText>
+//         <br/>
+//          <FuzzyText 
+//             baseIntensity={0.2} 
+//             >
+//             {pathName}
+//         </FuzzyText>
 
+//     </div>
+// }
+
+export default function Page(){
+    return <div>
+        page is not exist
     </div>
 }
